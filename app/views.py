d3c9d2179dbe82078b6b44024e3a967892346f47
Andrew Craighead <andrewcraighead@gmail.com>
2017-04-05 23:17:14 -0500

    almost there

--- app/views.py
+++ app/views.py
@@ -1,24 +1,34 @@
 from flask import Flask, request, jsonify, escape
-from app import app
+from app import app, db
 
 
 @app.route('/')
 @app.route('/index')
 @app.route('/version')
 def api_index():
-    return "Connected to octagon API version: 0.2"
+    return "Connected to octagon API version: 0.5"
 
-@app.route('/<fid>')
+
+@app.route('/fid/<fid>')
 def api_fid(fid):
-    return "Hello {}!".format(fid)
+    # TODO: return JSON record of all fighter stats from DB by fighter ID.
+    return 'Fighter ID: {}'.format(fid)
 
 
-@app.route('/name', methods=['POST'])
-def api_name():
-    _error = None
-    _name = None
-    try:
-        _name = request.get_json().get('name')
-    except:
-        _error = '#### ERROR in api_name()'
-    return jsonify({'name': _name, 'id': 1, 'error': _error})
+@app.route('/name/<name>')
+def api_name(name):
+    print(db)
+    # TODO: return JSON record of all fighter stats from DB by name.
+    return 'Fighter Name: {}'.format(name)
+
+
+
+# @app.route('/name', methods=['POST'])
+# def api_name():
+#     _error = None
+#     _name = None
+#     try:
+#         _name = request.get_json().get('name')
+#     except:
+#         _error = '#### ERROR in api_name()'
+#     return jsonify({'name': _name, 'id': 1, 'error': _error})
