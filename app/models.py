daf07f1b8bd81594d35599536db6c766c5272e53
Sam Kreter <samkreter@gmail.com>
2017-04-17 20:55:17 -0500

    working with multiple sessions

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy.orm import mapper, sessionmaker, clear_mappers
 from app import app
 
 
@@ -9,9 +9,9 @@ class Fighters(object):
 
 def loadsession():
     """"""
+    clear_mappers()
     db_path = app.config['SQLALCHEMY_DATABASE_URI']
     engine = create_engine(db_path, echo=False)
-
     metadata = MetaData(engine)
     fighters = Table('fighters', metadata, autoload=True, autoload_with=engine, schema='octagon')
     mapper(Fighters, fighters)
